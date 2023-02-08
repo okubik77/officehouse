539e52935730a11281c24ca4ef3cbe3e32a0c285
okubik77 <62718117+okubik77@users.noreply.github.com>
2023-02-08 09:09:09 +0100

    Add files via upload

---
+++ _footer.php
@@ -0,0 +1,17 @@
+<footer id="footer">
+    <div class="container">
+        <div class="content">
+            <img src="images/logo-footer.png" alt=""> 
+            <p><a href="#" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="#" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></p>
+            <p>DT-holding a.s.<br>
+            České mládeže 1096<br>
+            460 06 Liberec</p>
+        </div>
+    </div>
+</footer>
+<script src="js/custom.js?v=<?php echo rand(567,6789);?>"></script>
+<script src="js/core.min.js?v=1"></script>
+<script src="js/script.js?v=1"></script>
+</body>
+</html>
